a3db08d636782b373fa104b325defe0c308cdeaf
Shailendra47 <43498726+Shailendra47@users.noreply.github.com>
2019-08-26 15:03:20 +0530

    Matrix virus

---
+++ MatrixVirus.bat
@@ -0,0 +1,7 @@
+@echo off
+color Oa
+title---------------------Fake Matrixx Virus----------------------
+:top
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+goto top 
+
